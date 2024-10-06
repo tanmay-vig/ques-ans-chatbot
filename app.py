0741af9759eaf90d9b35c01e8068017b7cb04d39
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2024-10-06 07:55:27 +0000

    ques ans chatbot

--- app.py
+++ app.py
@@ -10,20 +10,34 @@ genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
 
 # Fuction to load gemini pro model and get responses
 model = genai.GenerativeModel("gemini-pro")
+chat = model.start_chat(history = [])
+
 def get_gemini_response(question):
-    response = model.generate_content(question)
-    return response.text
+    response = chat.send_message(question , stream=True)
+    return response
 
 # Streamlit app 
 st.set_page_config(page_title="Q&A Demo")
 
 st.header("Gemini LLM Application")
 
+#Initialize session state for Chat History if it doesn't exist
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
 input = st.text_input("Input : " , key="input")
 submit = st.button("Ask the question")
 
 # When submit is clicked 
-if submit:
+if submit and input:
     response = get_gemini_response(input)
-    st.subheader("The Response is:")
-    st.write(response)
+    # Add user query and response to session chat history
+    st.session_state['chat_history'].append(("You" , input))
+    st.subheader("The response is :")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot" , chunk.text))
+
+st.subheader("The chat history is :")
+for role,text in st.session_state['chat_history']:
+    st.write(f" {role}: {text} ")
